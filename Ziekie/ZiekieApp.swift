02b5956462967309fdb48c725743a4d25d942b0a
Suzanne <suzanne@toolsforpedagogy.com>
2023-02-13 16:47:10 +0100

    Initial Commit

---
+++ Ziekie/ZiekieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZiekieApp.swift
+//  Ziekie
+//
+//  Created by Suzanne Lustenhouwer on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZiekieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
